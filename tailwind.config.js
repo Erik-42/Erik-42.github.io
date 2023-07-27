acf0b78d230309e051a9c5eed9a7b26a9f24a008
Erik-42 <mesen.erik@gmail.com>
2023-07-27 12:17:16 +0200

    MajConfigetCV

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,12 @@
-/** @type {imort('tailwindcss').config} */
+/** @type {import('tailwindcss').config} */
 const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
     purge: {
         preserveHtmlElements: false,
-        content: ['./*.html,.js'],
+        content: ['./src/**/*{.html,.js,.jsx}'],
     },
     darkmode: false,
-    content: ["./**/*.{html,js}"],
-
+    content: { relative: true, files: ['./src/**/*{.html,.js,.jsx}'] },
     theme: {
         extend: {
             fontFamily: {
@@ -31,6 +30,7 @@ module.exports = {
         // require('@tailwindcss/forms'),
         // require('@tailwindcss/typography'),
         // require('@tailwindcss/aspect-ratio'),
-        // require('tailwindcss-children')
+        // require('tailwindcss-children'),
+        // require('@tailwindcss/line-clamp@latest')
     ],
 }
